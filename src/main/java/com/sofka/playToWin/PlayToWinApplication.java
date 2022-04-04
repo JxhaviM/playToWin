c35249efd74beaa4709bed952b6b24e05bbf5151
JxhaviM <jxhavi@gmail.com>
2022-04-04 13:30:23 -0500

    Creacion de estructura del proyecto

---
+++ src/main/java/com/sofka/playToWin/PlayToWinApplication.java
@@ -0,0 +1,13 @@
+package com.sofka.playToWin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayToWinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayToWinApplication.class, args);
+	}
+
+}
